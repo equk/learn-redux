9240cf05887bb46ff7b4ce3299f868945235595c
equk <equk@users.noreply.github.com>
2019-05-10 14:31:10 +0100

    added image and props to single by importing & reusing photo component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Photo from './Photo';
+// import comments
 
 const Single = React.createClass({
   render() {
-    return <div className="single-photo">I'm the single</div>;
+    const i = this.props.posts.findIndex(
+      post => post.code === this.props.params.postId
+    );
+    const post = this.props.posts[i];
+    console.log(post);
+    return <Photo i={i} post={post} {...this.props} />;
   },
 });
 
